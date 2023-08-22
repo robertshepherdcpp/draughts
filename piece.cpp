a3a6cf4f6acd0c257712365b2667a7e7a9f601b1
rshepherdcpp <113625774+robertshepherdcpp@users.noreply.github.com>
2023-08-22 12:30:02 +0100

    _

--- piece.cpp
+++ piece.cpp
@@ -1,7 +1,50 @@
 #include "piece.h"
+#include "Position.h"
 
 piece::piece(std::string filename, Position position)
 {
 	piece_texture.loadFromFile(filename);
 	piece_sprite.setTexture(piece_texture);
+
+    int x = position.m_x;
+    int y = position.m_y;
+    while ((x >= 0) && (x <= 7) && (y >= 0) && (y <= 7))
+    {
+        x += 1;
+        y += 1;
+        positions.push_back(Position(x, y));
+    }
+
+    x = position.m_x;
+    y = position.m_y;
+
+    while ((x >= 0) && (x <= 7) && (y >= 0) && (y <= 7))
+    {
+        x -= 1;
+        y -= 1;
+        positions.push_back(Position(x, y));
+    }
+
+    x = position.m_x;
+    y = position.m_y;
+
+    while ((x >= 0) && (x <= 7) && (y >= 0) && (y <= 7))
+    {
+        x += 1;
+        y -= 1;
+        positions.push_back(Position(x, y));
+    }
+
+    x = position.m_x;
+    y = position.m_y;
+
+    while ((x >= 0) && (x <= 7) && (y >= 0) && (y <= 7))
+    {
+        x -= 1;
+        y += 1;
+        positions.push_back(Position(x, y));
+    }
+
+    x = position.m_x;
+    y = position.m_y;
 }
